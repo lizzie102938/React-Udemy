4eaff7607dad567a3d2dd1c149655db79cd55dd1
lizzie lerwill <lizzielerwill@hotmail.com>
2022-04-06 11:47:16 +0200

    adding clicker

---
+++ clicker_exercise/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import Clicker from './Clicker';
+
+function App() {
+  return (
+    <div className="App">
+      <Clicker />
+    </div>
+  );
+}
+
+export default App;
